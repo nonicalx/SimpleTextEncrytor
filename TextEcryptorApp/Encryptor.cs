5fa44092ec1f5e30c96f3914877e4d29554a9df6
nonicalx <nonicalx@gmail.com>
2019-04-26 22:56:10 +0100

    Made solution into a OOP solution

---
+++ TextEcryptorApp/Encryptor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEcryptorApp
+{
+
+    class Encryptor
+    {
+        readonly char[] alphabets = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '=', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '[', ']', '\\', '{', '}', '|', ';', '\'', ':', '"', '\n', ',', '.', '/', '<', '>', '?','\t',' ' };
+
+        /// <summary>
+        /// Gets the string from the text file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private string GetString(string path)
+        {
+            string wordToEncrypt = "";
+            using (StreamReader streamReader = new StreamReader(path, Encoding.UTF8))
+            {
+                wordToEncrypt = streamReader.ReadToEnd();
+            }
+            return wordToEncrypt;
+        }
+
+        /// <summary>
+        /// Encrypts the text using a simple encryption formula
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public string Encrypt(string path)
+        {
+            string wordToEncrypt = GetString(path);
+            int value = 0;
+            int hashValue = 0;
+            string encrytedValue = "";
+
+            for (int i = 0; i < wordToEncrypt.Length; i++)
+            {
+                for (int j = 0; j < alphabets.Length; j++)
+                {
+                    if (wordToEncrypt.ToLower()[i] == alphabets[j])
+                    {
+                        value = j + 1;
+                        if (value % 2 == 0)
+                        {
+                            hashValue = value / 2;
+                            encrytedValue += "e" + hashValue.ToString();
+                        }
+                        else
+                        {
+                            hashValue = (value * 3) + 1;
+                            encrytedValue += "o" + hashValue.ToString();
+                        }
+                    }
+                }
+            }
+
+            return encrytedValue;
+        }
+    }
+}
--- TextEcryptorApp/Program.cs
+++ TextEcryptorApp/Program.cs
@@ -11,54 +11,15 @@ namespace TextEcryptorApp
     {
         static void Main(string[] args)
         {
-            string text = GetString(@"C:\Users\USER\Desktop\testSubject.txt");
-
-            string encrytedResult = Encrypt(text);
+            Encryptor encryptor = new Encryptor();
+            string path = @"C:\Users\USER\Desktop\testSubject.txt";
+            string encrytedResult = encryptor.Encrypt(path);
             Console.WriteLine(encrytedResult);
 
             Console.ReadLine();
         }
 
-        static string GetString(string path)
-        {
-            string wordToEncryt = "";
-            using (StreamReader streamReader = new StreamReader(path, Encoding.UTF8))
-            {
-                 wordToEncryt = streamReader.ReadToEnd();
-            }
-            return wordToEncryt;
-        }
 
-        static string Encrypt(string wordToEncrypt)
-        {
-
-            char[] alphabets = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',',' ','#','\'','@'};
-            int value = 0;
-            int hashValue = 0;
-            string encrytedValue = "";
-
-            for (int i = 0; i < wordToEncrypt.Length; i++)
-            {
-                for (int j = 0; j < alphabets.Length; j++)
-                {
-                    if (wordToEncrypt.ToLower()[i] == alphabets[j])
-                    {
-                        value = j + 1;
-                        if (value % 2 == 0)
-                        {
-                            hashValue = value / 2;
-                            encrytedValue += "e" + hashValue.ToString();
-                        }
-                        else
-                        {
-                            hashValue = (value * 3) + 1;
-                            encrytedValue += "o" + hashValue.ToString();
-                        }
-                    }
-                }
-            }
-
-            return encrytedValue;
-        }
+        
     }
 }
